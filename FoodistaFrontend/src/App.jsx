546324de49a72ff9b4108b91ea43e1b24c719eaf
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-04-14 21:23:05 -0400

    update jenkinsfile

--- FoodistaFrontend/src/App.jsx
+++ FoodistaFrontend/src/App.jsx
@@ -1,8 +1,8 @@
-import {BrowserRouter, Route, Routes, useLocation} from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import * as React from "react";
-import {useState} from "react";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
+import { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import getLPTheme from "./pages/getLPTheme.js";
 import Home from "./pages/Home.jsx";
 import SignUp from "./pages/SignUp.jsx";
@@ -13,45 +13,49 @@ import NewPost from "./pages/NewPost.jsx";
 import ShowPost from "./pages/ShowPost.jsx";
 import EditPost from "./pages/EditPost.jsx";
 
-function AppRoutes({mode, toggleColorMode}) {
-    const location = useLocation();
-    const [fullName, setFullName] = useState("");
+function AppRoutes({ mode, toggleColorMode }) {
+  const location = useLocation();
+  const [fullName, setFullName] = useState("");
 
-    return (
-        <>
-
-            {location.pathname !== "/signup" && location.pathname !== "/signin" && (
-                <Navbar mode={mode} toggleColorMode={toggleColorMode} setFullName={setFullName} fullName={fullName}/>
-            )}
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/signup" element={<SignUp/>}/>
-                <Route path="/signin" element={<SignIn/>}/>
-                <Route path="/myblog" element={<MyBlog fullName={fullName}/>}/>
-                <Route path="/newpost" element={<NewPost/>}/>
-                <Route path="/post" element={<ShowPost/>}/>
-                <Route path="/edit" element={<EditPost/>}/>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      {location.pathname !== "/signup" && location.pathname !== "/signin" && (
+        <Navbar
+          mode={mode}
+          toggleColorMode={toggleColorMode}
+          setFullName={setFullName}
+          fullName={fullName}
+        />
+      )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/myblog" element={<MyBlog fullName={fullName} />} />
+        <Route path="/newpost" element={<NewPost />} />
+        <Route path="/post" element={<ShowPost />} />
+        <Route path="/edit" element={<EditPost />} />
+      </Routes>
+    </>
+  );
 }
 
 function App() {
-    const [mode, setMode] = React.useState("light");
-    const LPtheme = createTheme(getLPTheme(mode));
+  const [mode, setMode] = React.useState("light");
+  const LPtheme = createTheme(getLPTheme(mode));
 
-    const toggleColorMode = () => {
-        setMode((prev) => (prev === "dark" ? "light" : "dark"));
-    };
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
 
-    return (
-        <ThemeProvider theme={LPtheme}>
-            <CssBaseline/>
-            <BrowserRouter basename="/comp313-003-Team-1-W24">
-                <AppRoutes mode={mode} toggleColorMode={toggleColorMode}/>
-            </BrowserRouter>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={LPtheme}>
+      <CssBaseline />
+      <BrowserRouter basename="/">
+        <AppRoutes mode={mode} toggleColorMode={toggleColorMode} />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- FoodistaFrontend/vite.config.js
+++ FoodistaFrontend/vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/comp313-003-Team-1-W24",
+  base: "/",
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
